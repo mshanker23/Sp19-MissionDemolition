51cd367b021a51a802aa048012896147a3f1efd5
Meghna Shanker <mshanker823@gmail.com>
2019-02-21 11:48:11 -0500

    Updated FollowCam
    Updated FollowCam to reset

--- Assets/_Scripts/FollowCam.cs
+++ Assets/_Scripts/FollowCam.cs
@@ -13,7 +13,7 @@ public class FollowCam : MonoBehaviour
     [Header("Set Dynamically")]
     public float camZ;
 
-    private void Awake()
+    void Awake()
     {
         camZ = this.transform.position.z;
     }
@@ -21,13 +21,33 @@ public class FollowCam : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (POI == null) return;
-        Vector3 destination = POI.transform.position;
+        Vector3 destination;
+
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            destination = POI.transform.position;
+            if (POI.tag == "Projectile")
+            {
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    POI = null;
+                    return;
+                }
+            }
+        }
+
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
         destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
         Camera.main.orthographicSize = destination.y + 10;
+
     }
+
+
 }
