22ac0019d33cc8e654410a7118a7bb64e6492c3d
Meghna Shanker <mshanker823@gmail.com>
2019-02-21 12:40:01 -0500

    Created Goal and ProjectileLine
    Added a line to the projectile and made goal turn green upon entry

--- Assets/_Scripts/Slingshot.cs
+++ Assets/_Scripts/Slingshot.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour
 {
     // Start is called before the first frame updat
-
+    static private Slingshot S;
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
     public float velocityMult = 8f;
@@ -16,9 +16,19 @@ public class Slingshot : MonoBehaviour
     public GameObject projectile;
     public bool aimingMode;
     private Rigidbody projectileRigidbody;
-    
+
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
     private void Awake()
     {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
